7bc28a8b42afb90038708d78f68accab6ad032eb
Mohamed Abdelgaber <mohamedabdelgaber247@gmail.com>
2025-10-23 00:14:50 +0300

    [Relay Control Driver Version 1.0] [ Control ON/OFF state of relays via clean APIs. Provide relay status feedback. Support multiple relays via configuration.
    Implemented APIs:
    void hRelay_Init(uint8_t relayId);
    void hRelay_On(uint8_t relayId);
    void hRelay_Off(uint8_t relayId);
    void hRelay_Toggle(uint8_t relayId);
    uint8_t hRelay_GetStatus(uint8_t relayId);
    Handle active state logic (Active HIGH / Active LOW via config).
    
    ]
    [Feat] #16

--- Hal/RelayControl/RELAY_Private.h
+++ Hal/RelayControl/RELAY_Private.h
@@ -12,26 +12,11 @@
 #ifndef _HRELAY_PRIVATE_H_
 #define _HRELAY_PRIVATE_H_
 
-/* --------------------------------------------------------------------------
-*                               Include LIB
-* --------------------------------------------------------------------------*/
-
-#include <stdint.h>
-#include "RELAY_Config.h"
-#include "../../Mcal/DIO/DIO_Interface.h"
-#include "../../Common/Macros.h"
-
-
 /**
  * @note  This file is currently empty and not used in the RELAY program.
  *        Any internal macros, constants, or private definitions may be added
  *        here in future versions if needed.
 */
-
-
-
-
-
 
 
 #endif /*_HRELAY_PRIVATE_H_*/
