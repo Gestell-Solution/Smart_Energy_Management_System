3adb265d7890ba1162dc648ab4711124fc38112a
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-10-22 21:55:13 +0300

    Doing last review point

--- Hal/PushButton/Notes OF Review.c
+++ Hal/PushButton/Notes OF Review.c
@@ -3,4 +3,6 @@
 // 3- The GIE Enable is not included to the files 
 // Try to revise The doxygen Comments again ,I am not talking about making modules of your code like input parts But I find there is errors in
 //writing the Comments and lastly just try to read you comments to know if you understand and keep going
-
+// in the ISR action you are using the If Condition in toggling the value but there is better way 
+// Val ^= 1; this will toggle the Value directly 
+// For more serach on XOR 
