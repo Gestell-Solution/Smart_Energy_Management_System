ed38295dfb501c9260c81614b8ed64d876fd99f4
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-10-22 15:16:50 +0300

    [LCD_Driver finishing the Code Not Tested ] [-Config ,private,Interface  header Files is done  -Lcd_Driver Program is done too.
     ]
    [Feat]   #17

--- Hal/LCD/LCD_Config.h
+++ Hal/LCD/LCD_Config.h
@@ -11,4 +11,50 @@
  */
 #ifndef _LCD_CONFIG_H_
 #define _LCD_CONFIG_H_
+#include "LCD_Private.h"
+#include "../../Mcal\DIO\DIO_Interface.h"
+/** 
+ * @def Clear_Line_String 
+ * @brief String used to clear a line on the LCD.
+ */
+#define  Clear_Line_String "               "
+
+/** 
+ * @def LCD_Group 
+ * @brief Defines the group used for LCD control.
+ */
+#define LCD_Group GroupC
+
+/** 
+ * @def Upper_Nibble_Masking 
+ * @brief Extracts the upper nibble (4 bits) from a register.
+ */
+#define Upper_Nibble_Masking(Reg) ((Reg>>4)&0x0F)
+
+/** 
+ * @def Lower_Nibble_Masking 
+ * @brief Extracts the lower nibble (4 bits) from a register.
+ */
+#define Lower_Nibble_Masking(Reg) ((Reg)&0x0F)
+
+
+/** 
+ * @def NumOfRows 
+ * @brief Total number of display rows.
+ */
+#define NumOfRows 4
+
+/** 
+ * @def Rows_Addresses_Array 
+ * @brief Array of starting addresses for each LCD row.
+ */
+#define Rows_Addresses_Array {Row_1_Start,Row_2_Start,Row_3_Start,Row_4_Start}
+
+
+/** 
+ * @def Array_Num_Of_Digits 
+ * @brief Maximum number of digits for a 32-bit unsigned integer.
+ */
+#define Array_Num_Of_Digits 10 
+
 #endif
