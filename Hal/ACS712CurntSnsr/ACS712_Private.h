b50b54e776a548c638e40ef4516f82617d6572bc
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-10-23 22:22:22 +0300

    [Current sensor real coding] [Stating out the files] [Feat]   #15

---
+++ Hal/ACS712CurntSnsr/ACS712_Private.h
@@ -0,0 +1,11 @@
+/**
+ * @file        acs712_private.h
+ * @brief       Internal definitions for ACS712 driver.
+ * @details     Provides static declarations and internal helper macros used only within the ACS712 driver implementation.
+ * @version     1.0.0
+ * @author      Mohamed Diaa <mohammediaato@gmail.com> @Mohamed-Diaa-ES
+ * @date        2025-10-23
+ * @copyright   Copyright (c) 2025, Gestell Company
+ */
+ #ifndef _ACS712_PRIVATE_H_
+#endif
