723db9df6d1915d954aba7bd7d49f279a42c8b2a
basmakh45 <basma.abdelhalim04@eng-st.cu.edu.eg>
2025-10-24 17:11:15 +0300

    Add Buzzer driver files (Config, Interface, Private, Program)

---
+++ Hal/Buzzer/Buzzer_Config.h
@@ -0,0 +1,28 @@
+/**
+ *@file Buzzer_Config.h
+ *@brief Configuration file for the Buzzer driver.
+ *@details
+ * Defines the buzzer pin connections and timing parameters for different buzzer modes
+ * such as single beep, periodic beep, and alert beep.
+
+ * @version 1.0.0
+ * @author <Basma khaled> <basmak55@gmail.com>
+ * @copyright Copyright (c) 2025 , Gestell Company
+ */
+#ifndef BUZZER_CONFIG_H_
+#define BUZZER_CONFIG_H_
+#include"../../Mcal/DIO/DIO_Interface.h"
+/**
+ * @defgroup
+ * @brief Configurable Pin for buzzer
+ */
+#define BUZZER_PORT     GroupB
+#define BUZZER_PIN      PIN0
+/**
+ * @defgroup
+ * @brief beep durations in milliseconds
+ */
+#define BUZZER_BEEP_DURATION       500
+#define BUZZER_PERIODIC_DURATION   1500
+#define BUZZER_ALERT_DURATION        50
+#endif /* BUZZER_CONFIG_H_ */
---
+++ Hal/Buzzer/Buzzer_Private.h
@@ -0,0 +1,6 @@
+/**
+ * @file Buzzer_Private.h
+ * @version 1.0.0
+ * @author <Basma khaled> <basmak55@gmail.com>
+ * @copyright Copyright (c) 2025 , Gestell Company
+ */
