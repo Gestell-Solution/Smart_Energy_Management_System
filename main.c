9562756046f588af6d5edfd6200e0b3f3e67237a
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-12-05 02:24:04 +0200

    adding the changes of the Master

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Tests/Uart/uartTest.h"
 void main()
 {
@@ -5,5 +6,37 @@ void main()
     while(1)
     {
         
+=======
+<<<<<<< HEAD
+// AVR peripheral libraries
+//#include <avr/interrupt.h> // Required for sei()
+#include <util/delay.h>    // Required for _delay_ms()
+=======
+#include "App/CommunicationManager/App_CommManager.h"
+#include "Mcal/Timer0/TIMER0_Interface.h"
+// Include your HAL headers here (e.g., hBT.h, mTIMER.h)
+>>>>>>> fafca73 (Fixing Some logic of using static data logic)
+
+int main(void)
+{
+
+<<<<<<< HEAD
+    mGIE_Enable();
+        SetBit(SREG_Reg,7); 
+=======
+    App_CommManager_Init(); 
+>>>>>>> fafca73 (Fixing Some logic of using static data logic)
+
+
+    uint8_t boot_msg[] = { 'R', 'e', 'a', 'd', 'y' };
+    App_CommManager_SendFrame( boot_msg,0x00, 5);
+
+    while (1)
+    {
+        App_CommManager_Task();
+
+
+        mTIMER0_Delay_ms(50);
+>>>>>>> 6660dab (adding the changes of the Master)
     }
 }
