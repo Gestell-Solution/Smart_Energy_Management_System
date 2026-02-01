c0e2d627bd559434b13d19e1837ff0911391133c
Ahmed Ashraf <ahmedashraf2022222@gmail.com>
2026-02-01 19:14:39 +0200

    [Added assignation of RMS values to System Data Structure and fixed payload size in communication manager] [INT]

--- Firmware/Embedded/Src/App/MeasurementEngine/MeasurementEngine_Progarm.c
+++ Firmware/Embedded/Src/App/MeasurementEngine/MeasurementEngine_Progarm.c
@@ -17,7 +17,7 @@
  *                                 Includes
  *============================================================================*/
 #include "MeasurementEngine_Interface.h"
-
+#include "../../Common/SystemDataManager/SystemDataManager.h"
 
 /*============================================================================
  *                                 Private Variables
@@ -74,6 +74,9 @@ void ME_Update(void)
 #endif
     /* Energy accumulation: Energy (J) = Power (W) * Time (s) */
     ME_Energy += ME_Active_Power * ME_SAMPLE_INTERVAL;
+g_SystemData.Voltage_RMS = ME_Vrms;
+g_SystemData.Current_RMS = ME_Irms;
+g_SystemData.Power= ME_Active_Power;
 }
 
 /**
